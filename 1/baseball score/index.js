1b5c7a0b921be08e126667973d6db21c907b8d69
barnobarno666 <barnoahmed666@gmail.com>
2023-06-28 20:00:20 +0600

    hy

---
+++ 1/baseball score/index.js
@@ -0,0 +1,28 @@
+let countH=0
+document.getElementById('scorehome').innerText=countH
+function one(){
+    countH=countH+1
+    document.getElementById('scorehome').innerText=countH
+}
+function two(){
+    countH=countH+2
+    document.getElementById('scorehome').innerText=countH
+}
+function three(){
+    countH=countH+3
+    document.getElementById('scorehome').innerText=countH
+}
+let countG=0
+document.getElementById('scoreguest').innerText=countH
+function oneG(){
+    countG=countG+1
+    document.getElementById('scoreguest').innerText=countG
+}
+function twoG(){
+    countG=countG+2
+    document.getElementById('scoreguest').innerText=countG
+}
+function threeG(){
+    countG=countG+3
+    document.getElementById('scoreguest').innerText=countG
+}
